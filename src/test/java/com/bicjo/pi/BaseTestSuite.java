30430b3e495785f6f52690602f64e97bd1c0238e
bicjo <jovenbico@gmail.com>
2016-05-31 14:10:20 +0800

    base junit testSuite

---
+++ src/test/java/com/bicjo/pi/BaseTestSuite.java
@@ -0,0 +1,12 @@
+package com.bicjo.pi;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.bicjo.pi.myip.WhatIsMyIpTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ WhatIsMyIpTest.class })
+public class BaseTestSuite {
+
+}
